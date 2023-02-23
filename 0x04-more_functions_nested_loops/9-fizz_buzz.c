09e72ee4c2441ede0769aabd3c0995e4ef405552
challomark <challomark@gmail.com>
2023-02-23 12:24:48 +0300

    Corrected error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,7 +20,7 @@ int main(void)
 		{
 			if (i == 100)
 				printf("Buzz");
-			else 
+			else
 				printf("Buzz ");
 		}
 		else if (i % 3 == 0)
@@ -32,7 +32,6 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
-	
 	printf("\n");
 
 	return (0);
